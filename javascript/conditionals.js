046d05f653473c59fada40925af4e9e4643c5704
Marina <marina@Alexs-MacBook-Pro.local>
2021-05-03 22:06:38 +0200

    First commit from home

---
+++ javascript/conditionals.js
@@ -0,0 +1,160 @@
+//prvi zadatak
+
+// var a = 3;
+// var b = -7;
+// var c = 2;
+// var product = a * b * c;
+
+// if(product > 0){
+//     console.log("The sign is +");
+// }else{
+//     console.log("The sig is -");  
+// }
+
+
+//drugi zadatak
+
+/*var a = -5;
+var b = -4;
+ var c =76;
+ var d = 0;
+ var e = -1;
+
+var trenutnoNajveciBr = a;
+
+if( b > trenutnoNajveciBr){
+     trenutnoNajveciBr = b
+}
+ if( c > trenutnoNajveciBr){
+    trenutnoNajveciBr = c
+}
+ if( d > trenutnoNajveciBr){
+    trenutnoNajveciBr = d
+}
+if( e > trenutnoNajveciBr){
+     trenutnoNajveciBr = e
+}
+console.log(trenutnoNajveciBr); */ 
+
+
+//treci zadatak
+
+// var a =0;
+// var b = -1;
+// var c = 4;
+
+// var najveci = a;
+// var srednji = a;
+// var najmanji = a;
+
+// if( b > najveci){
+//     najveci = b
+// }
+
+// if( c > najveci){
+//     najveci = c
+// }
+
+
+// if( b < najmanji){
+//     najmanji = b
+//     }
+    
+// if( c < najmanji){
+//     najmanji = c
+//     }
+ 
+
+//  if( a === najveci && b === najmanji){
+//      srednji = c
+//  }else if(b === najveci && c === najmanji){
+//      srednji = a
+//  }else if(c === najveci && a === najmanji){
+//      srednji = b
+//  }
+//  console.log(najveci + "," + srednji + "," + najmanji);
+
+
+//cetvrti zadatak
+
+//  var  a = 10;
+
+//  if(typeof a === "number" && a%2 === 0){
+//      console.log(a/2);
+//  }else{
+//      console.log(("X"));
+//  }
+
+
+ //peti zadatak
+
+//  var a = 3;
+//  var b = 6;
+//  var najveci = a;
+
+//  if(b > najveci){
+//      najveci = b  
+//      console.log(b);
+     
+//  }else{
+//      console.log(a);
+     
+//  }
+
+
+//sedmi zadatak
+
+// var givenNUmber = 32;
+ 
+// if(givenNUmber > 13){
+//     console.log((givenNUmber - 13) * 2)
+// }else{
+//     console.log("Greskaaa");
+// }
+
+
+//osmi zadatak
+
+// var a = 12;
+// var b = 5;
+// var c = 8;
+// var d = 8;
+
+
+// if( a === b){
+//     console.log((a + b)* 3)  
+// }else{
+//     console.log(a + b);
+// }
+
+// if( c === d){
+//     console.log((c + d)* 3)  
+// }else{
+//     console.log(c + d);
+// }
+
+ //deveti zadatak
+
+// var a = 40;
+// var b = 10;
+
+// if(a === 50 || b === 50){
+//     console.log("true"); 
+// }
+// if(a + b === 50){
+//     console.log("True"); 
+// }
+
+
+//deseti zadatak
+
+// var a = 13;
+
+// if(a > 20 && a < 100){
+//     console.log("20 ~ 100");
+// }else if(a > 100 && a < 400){
+//     console.log("100 ~ 400"); 
+// }else if(a < 20){
+//     console.log(" - ");
+// }
+
